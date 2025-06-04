8f0dbebabe88b3e6597268970f3f43cc886abf2f
Omar Muhammad <cds.omarmuhammad92694@alexu.edu.eg>
2025-06-04 12:48:42 +0300

    Move lab2 to dedicated branch

--- lab4/articles/app/models/ability.rb
+++
@@ -1,9 +0,0 @@
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-      can :read, Article
-      can :report, Article
-      can [ :create, :update, :destroy ], Article, user_id: user.id
-  end
-end
